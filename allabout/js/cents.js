4313724d6b11c0f752bc95531b3c86b526d0746c
thsoin <thomas.schmidt.info@gmx.ch>
2019-01-20 17:27:06 -0600

    Add files via upload

---
+++ allabout/js/cents.js
@@ -0,0 +1,49 @@
+//var amount = document.getElementById('amount');
+var current;
+
+let eink = 130000; // Ausgangsgehalt
+let seck = 31535999; // vom Internet2019 - nicht ganz genau ist 365*24*60*60 Sekunden pro Jahr
+let cent = eink / seck; // Einkommen pro Sekunde
+
+var dateObj = new Date();
+var dateInit = new Date(dateObj.getFullYear(), "0", "1");
+dateInit = Math.round(dateInit.getTime() / 1000);
+
+var dateCurrent = Math.round(dateObj.getTime() / 1000);
+let sekJahr = dateCurrent - dateInit; // Sekunden dieses Jahr bis jetzt
+let verd = cent * sekJahr; // verdient bis jetzt = sekunden dieses Jahr mal cent pro sekunden
+
+var current = verd;
+
+function update() {
+  //amount.innerText = formatMoney(current);
+  var numb = current;
+  numb = numb.toFixed(3); // runden auf 3 Kommastellen
+  clock.setValue(numb);
+};
+
+setInterval(function() {
+  current += cent;
+  update();
+}, 1000);
+
+/*
+// nur zum Zahlen darstellen ohne FlipClock
+function formatMoney(amount) {
+  var dollars = Math.floor(amount).toString().split('');
+  var cents = (Math.round((amount % 1) * 100) / 100).toString().split('.')[1];
+  if (typeof cents == 'undefined') {
+    cents = '00';
+  } else if (cents.length == 1) {
+    cents = cents + '0';
+  }
+  var str = '';
+  for (i = dollars.length - 1; i >= 0; i--) {
+    str += dollars.splice(0, 1);
+    if (i % 3 == 0 && i != 0) str += '';
+    //      if(i%3 == 0 && i != 0) str += ',';  für Komma 12,000 = 12000
+  }
+  return str + ',' + cents + " " + '€';
+
+}
+*/
